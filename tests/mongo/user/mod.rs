383091e18945264345f74cebcd94ed35471aa858
cowbear6598 <cowbear6598@gmail.com>
2023-01-13 18:45:25 +0800

    complete all test

--- tests/mongo/user/mod.rs
+++ tests/mongo/user/mod.rs
@@ -1,37 +1,36 @@
 extern crate web_server_practice;
 
 use actix_web::{App, web, test::{init_service, TestRequest, call_and_read_body_json}};
-use actix_web::dev::{Service, ServiceResponse};
 use web_server_practice::{mongo, mongo::controllers, mongo::models};
 
 #[actix_web::test]
 async fn should_mongo_user_controllers_success() {
+    let user = models::user::User {
+        name: "test".to_string(),
+        email: "cowbear6598@gmail.com".to_string(),
+    };
+
+    should_create_user_success(&user).await;
+    should_get_user_success(&user).await;
+}
+
+async fn should_create_user_success(user: &models::user::User) {
     let client = mongo::mongo_connect().await;
 
     let app = init_service(
         App::new()
             .app_data(web::Data::new(client))
             .service(controllers::user::add_user)
-            .service(controllers::user::get_user)
     ).await;
 
-    let user = models::user::User {
-        name: "test".to_string(),
-        email: "cowbear6598@gmail.com".to_string(),
-    };
-
-    should_create_user_success(&app, &user).await;
-}
-
-async fn should_create_user_success(app: &impl Service<Request, Response=ServiceResponse<_>, Error=_, Future=_> + Sized, user: &models::user::User) {
-    let user_create_req = TestRequest::post()
+    let req = TestRequest::post()
         .uri("/api/user/add")
         .set_json(&user)
         .to_request();
 
-    let user_create_res: models::Response<models::user::User> = call_and_read_body_json(&app, user_create_req).await;
+    let res: models::Response<models::user::User> = call_and_read_body_json(&app, req).await;
 
-    assert_eq!(user_create_res, models::Response {
+    assert_eq!(res, models::Response {
         status: 0,
         message: "ok".to_string(),
         data: models::user::User {
@@ -41,8 +40,30 @@ async fn should_create_user_success(app: &impl Service<Request, Response=Service
     });
 }
 
-async fn should_get_user_success() {
+async fn should_get_user_success(user: &models::user::User) {
+    let client = mongo::mongo_connect().await;
 
+    let app = init_service(
+        App::new()
+            .app_data(web::Data::new(client))
+            .service(controllers::user::add_user)
+            .service(controllers::user::get_user)
+    ).await;
+
+    let req = TestRequest::get()
+        .uri(&format!("/api/user/info/{}", &user.email))
+        .to_request();
+
+    let res: models::Response<models::user::User> = call_and_read_body_json(&app, req).await;
+
+    assert_eq!(res, models::Response {
+        status: 0,
+        message: "ok".to_string(),
+        data: models::user::User {
+            name: "test".to_string(),
+            email: "cowbear6598@gmail.com".to_string(),
+        },
+    });
 }
 
 
